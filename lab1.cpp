fc50c3bdfb76929078c42e77ad8355708dc593e2
krkastudent <pogosyan_da@edu.surgu.ru>
2026-09-16 17:12:06 +0500

    Исправил функции printArray и multiplyByTwo, где for заменён на range-based for

--- lab1.cpp
+++ lab1.cpp
@@ -1,29 +1,29 @@
 #include <iostream>
 using namespace std;
 
-void fillArray(int (&arr)[10]){
+void fillArray(int (&arr)[10]) {
     cout << "Введите 10 целых чисел: " << endl;
-    for (int i = 0; i < 10; i++){
+    for (int i = 0; i < 10; i++) {
         cin >> arr[i];
     }
 }
 
-void printArray(int (&arr)[10]){
-    for (int i = 0; i < 10; i++){
-        cout << arr[i] << " ";
+void printArray(int (&arr)[10]) {
+    for (auto& x : arr) {
+        cout << x << " ";
     }
     cout << "\n";
 }
 
-void swapElements(int (&arr)[10], int &first, int &second){
-    int a = arr[first];
+void swapElements(int (&arr)[10], int& first, int& second) {
+    int tmp = arr[first];
     arr[first] = arr[second];
-    arr[second] = a;
+    arr[second] = tmp;
 }
 
-void multiplyByTwo(int (&arr)[10]){
-    for (int i = 0; i < 10; i++){
-        arr[i] *= 2;
+void multiplyByTwo(int (&arr)[10]) {
+    for (int& x : arr) {
+        x *= 2;
     }
 }
 
@@ -36,10 +36,10 @@ int main() {
     int first = 0;
     int second = 9;
     swapElements(array, first, second);
-    cout << "Массив, после обмена первого и последнего элемента: ";
+    cout << "Массив после обмена первого и последнего элемента: ";
     printArray(array);
 
     multiplyByTwo(array);
-    cout << "Массив, после умножения всех элементов на два: ";
+    cout << "Массив после умножения всех элементов на два: ";
     printArray(array);
 }

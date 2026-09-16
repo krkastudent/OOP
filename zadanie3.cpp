232cfe123cd35dd4822f01a29576f32a091812db
krkastudent <pogosyan_da@edu.surgu.ru>
2026-09-16 19:14:01 +0500

    добавил функцию getElement и функцию для вывода элементов массива

--- zadanie3.cpp
+++ zadanie3.cpp
@@ -12,3 +12,24 @@ SafeArray createArray(int size) {
     arr.data = new int[size]{};
     return arr;
 }
+
+int& getElement(SafeArray& arr, int index) {
+    if (index < 0 || index >= arr.size) {
+        cout << "Ошибка: индекс " << index << " вне границ массива (размер " << arr.size << ")\n";
+        static int dummy = 0;
+        return dummy;
+    }
+    return arr.data[index];
+}
+
+void printSafe(const SafeArray& arr) {
+    cout << "Массив: ";
+    for (int i = 0; i < arr.size; i++) {
+        cout << arr.data[i] << " ";
+    }
+    cout << "\n";
+}
+
+int main(){
+    
+}
